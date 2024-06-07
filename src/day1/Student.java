e0205b0462f0e9548d85e4b1b40cac952090dba8
unknown <Rahul.Kumar@witty>
2024-06-07 12:48:01 +0530

    first commit

---
+++ src/day1/Student.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class Student {
+
+	public static void main(String[] args) {
+		//System.out.println("Welcome");
+		String i ="welcome";	
+		i.replace('e', 'r');
+		System.out.println(i.replace('e', 'r'));
+		
+
+	}
+
+}
